8f46ae921a2298f51175bc59dd563d9356107e94
Roger Enand <rogerenand@gmail.com>
2022-07-11 13:54:13 -0400

    added ability to track what room user is in

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-//const http = require('http').createServer();
-
-/*const io = require('socket.io')(http, {
-    cors: { origin: "*" ,methods: ["GET", "POST"]}
-});*/
 const randomWords = require('random-words');
 const io = require("socket.io")(process.env.PORT || 42630,{
     cors: {
@@ -12,13 +7,8 @@ const io = require("socket.io")(process.env.PORT || 42630,{
 });
 const waitingList = [];
 let roomList = [];
-let adminId = '';
 let clients;
 const roomPrefix = 'game-room-'
-const sendWaitingListSize = () => {
-    clients = io.sockets.adapter.rooms.get('waiting');
-    clients !== undefined ? io.to(adminId).emit('waitingCount',clients.size) : io.to(adminId).emit('waitingCount', 0);
-}
 
 setInterval(() => {
     const allRooms = io.sockets.adapter.rooms;
@@ -35,37 +25,56 @@ setInterval(() => {
 
 io.on('connection', (socket) => {
     const id = socket.id;
-    waitingList.push(id)
-    socket.join('waiting')
-    io.to(id).emit('joined','you joined the list')
-    if (adminId) {
-        sendWaitingListSize();
+    const myRoomList = [];
+    function joinARoom(roomName) {
+        socket.join(roomName)
+        myRoomList.push(roomName);
     }
+
+    function leaveARoom(roomName) {
+        socket.leave(roomName);
+        myRoomList.splice(myRoomList.findIndex((room) => {
+            return room === roomName;
+        }),1);
+    }
+    waitingList.push(id)
+    joinARoom('waiting');
+    io.to(id).emit('joined','you joined the list')
+
+    socket.on('getRoomCounts',(data) => {
+        const roomSize = io.sockets.adapter.rooms.get(`${roomPrefix}${data}`)?.size - 1 || 0;
+        io.to(id).emit('getRoomCounts',roomSize);
+    })
+
     socket.on('createRoom',() => {
         const roomCode = randomWords({ exactly: 3, join: '-' });
         io.to(id).emit('roomCode',roomCode);
-        socket.join(`${roomPrefix}${roomCode}`);
-        socket.join(`admins`);
+        console.log(myRoomList);
+        leaveARoom('waiting');
+        console.log(myRoomList);
+        joinARoom(`${roomPrefix}${roomCode}`)
+        joinARoom('admins')
+        console.log(myRoomList);
     })
+
     socket.on('joinNewRoom',(data) => {
-        console.log(data)
         socket.leave('waiting');
-        socket.join(`room-${data}`)
+        socket.join(`${roomPrefix}${data}`)
     })
-    socket.on('isAdmin', () => {
+
+    /*socket.on('isAdmin', () => {
             socket.leave('waiting');
             const index = waitingList.indexOf(adminId)
             if (index) {
                 waitingList.splice(index)
             }
             sendWaitingListSize();
-    });
+    });*/
+
     socket.on('adminControl', (data) => {
-        if (id !== adminId) {
-            return;
-        }
         socket.to('players').emit('roomCode',data)
     });
+
     socket.on('addAllToPlayers', async (data) => {
         const clients = await io.in("waiting").fetchSockets();
         if (clients !== undefined) {
@@ -76,7 +85,6 @@ io.on('connection', (socket) => {
                 io.to(client.id).emit('hasBeenChosen',true);
             })
             io.to('players').to('waiting').emit('hostHasChosen', true);
-            sendWaitingListSize();
         }
     });
 
@@ -85,15 +93,8 @@ io.on('connection', (socket) => {
         if (index) {
             waitingList.splice(index)
         }
-        if (id === adminId) {
-            adminId = '';
-
-        }
         socket.leave('players')
         socket.leave('waiting')
-        if (adminId) {
-            sendWaitingListSize();
-        }
     });
 });
 
